cfa52ca4f9f12870a6deb1578fe5deada3b057f5
harip0200 <harip0200@gmail.com>
2015-12-29 11:08:13 +0530

    The router page

---
+++ quizapp/app/router.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+const Router = Ember.Router.extend({
+  location: config.locationType
+});
+
+Router.map(function() {
+  this.route('quiz');
+  this.route('quiz' ,{path:"/quiz/:id/:id2"});
+  this.route('quiz' ,{path:"/quiz/:id/:id2"});
+  this.route("answer");
+  this.route('answer' ,{path:"/answer/:id/:id2"});
+
+});
+
+export default Router;
