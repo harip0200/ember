21a277d36df321a6ee960299506785d097132531
harip0200 <harip0200@gmail.com>
2015-12-31 10:18:11 +0530

    quiz project

--- quiz/app/routes/quiz.js
+++ quiz/app/routes/quiz.js
@@ -3,7 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model:function(que){
 
-    console.log(que.id)
     return this.store.all("quiz" ).get("content")[que.id];
   }
 });
